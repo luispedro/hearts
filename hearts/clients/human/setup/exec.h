18a5191c8eec5543ec0b75e851360cf6c27f91ac
luis_pedro <luis_pedro@174c993c-7530-0410-8f2a-93c0592b7a1f>
2003-03-13 23:49:11 +0000

    added include guards
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@33 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts/clients/human/setup/exec.h
+++ hearts/clients/human/setup/exec.h
@@ -1,6 +1,12 @@
+#ifndef DB_EXEC_H_INCLUDE_GUARD_
+#define DB_EXEC_H_INCLUDE_GUARD_
+
 #include <qstring.h>
 
 bool execute_server();
 bool execute_server(short port);
 bool execute_computer_client(QString name);
 
+
+
+#endif /* DB_EXEC_H_INCLUDE_GUARD_ */

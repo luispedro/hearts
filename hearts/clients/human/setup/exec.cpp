5ba5e3c19ec9a9da44fbe6f865b83634bc64edef
luis_pedro <luis_pedro@174c993c-7530-0410-8f2a-93c0592b7a1f>
2003-05-21 19:56:43 +0000

    Make this use the correct options. Especially the --wait-zero + --zero options.
    Output on the debug stream the exact options passed.
    
    
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@56 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts/clients/human/setup/exec.cpp
+++ hearts/clients/human/setup/exec.cpp
@@ -1,8 +1,22 @@
 #include "exec.h"
+#include "general/helper.h"
+#include "communication/constants.h"
+
 #include <kprocess.h>
 #include <qstring.h>
+#include <qstringlist.h>
 
 #include "communication/constants.h"
+
+
+std::ostream& operator<<(  std::ostream& s, const QValueList<QCString>& l )
+{
+	QValueList<QCString>::const_iterator it = l.begin();
+	for (  ; it != l.end(); ++it )
+		s << '\"' << ( *it ) << "\" "; 
+	return s;
+}
+
 
 bool execute_server()
 {
@@ -12,7 +26,12 @@ bool execute_server()
 bool execute_server( short port )
 {
 	KProcess p;
-	p << "heartsserver" << QString::number( port );
+	p << "heartsserver" 
+		<< "--tcp_port" << QString::number( port ) 
+		<< "--unix_port" << local_address
+		<< "--wait-zero";
+
+	LOG_PLACE() << '!' << p.args() << "!\n";
 	return p.start(KProcess::DontCare);
 }
 
@@ -20,7 +39,11 @@ bool execute_server( short port )
 bool execute_computer_client(QString name)
 {
 	KProcess p;
-	p << "heartscomputerclient" << "--playername" << name;
+	p << "heartscomputerclient"
+		<< "--playername" << name
+		<< "--address" << local_address
+		<< "--zero";
+	LOG_PLACE() << '!' << p.args() << "!\n";
 	return p.start(KProcess::DontCare);
 }
 

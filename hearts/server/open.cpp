d07c989bcae5e8036981388593a5766367b4b76a
luis_pedro <luis_pedro@174c993c-7530-0410-8f2a-93c0592b7a1f>
2003-05-21 18:23:01 +0000

    Indentation fixes.
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@43 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts/server/open.cpp
+++ hearts/server/open.cpp
@@ -39,63 +39,64 @@
 
 void set_sock_options(int fd)
 {		// FIXME: remove these options for a real, i.e. production version.
-		linger lin;
-		lin.l_onoff = 1;
-		lin.l_linger = 0;
-		setsockopt(fd,SOL_SOCKET,SO_LINGER,&lin,sizeof(lin));
+	linger lin;
+	lin.l_onoff = 1;
+	lin.l_linger = 0;
+	setsockopt(fd,SOL_SOCKET,SO_LINGER,&lin,sizeof(lin));
 }
 
 void open_connections()
 { // FIXME: this code kind of sucks, and it is too trusting 
-		int listen_tcp_fd = socket(PF_INET,SOCK_STREAM,0);
-		{
-			sockaddr_in addr;
-			memset(&addr,0,sizeof(addr));
-			addr.sin_family = AF_INET;
-			addr.sin_port = htons(options->tcp_port() );
-			addr.sin_addr.s_addr = htonl(INADDR_ANY);
-			set_sock_options(listen_tcp_fd);
-			if (bind(listen_tcp_fd,(sockaddr*)&addr,sizeof(addr)) < 0)
-					std::cerr << "Error in bind: " << strerror(errno) << ".\n";
-			listen(listen_tcp_fd,4);
-		}
-		int listen_local_fd = socket(PF_LOCAL,SOCK_STREAM,0);
-		{	
-			sockaddr_un addr;
-			memset(&addr,0,sizeof(addr));
-			addr.sun_family = AF_LOCAL;
-			unlink(options->unix_address() );
-			strcpy(addr.sun_path,options->unix_address() );
-			if (bind(listen_local_fd,reinterpret_cast<sockaddr*>(&addr),sizeof(addr)) < 0)
-					std::cerr << "Error in local bind: " << strerror(errno) << ".\n";
-			listen(listen_local_fd,4);
-		}
-		pollfd listen[2];
-		memset(listen,0,sizeof(listen));
-		enum { tcp, local };
-		listen[tcp].fd = listen_tcp_fd;
-		listen[tcp].events = POLLIN;
-		listen[local].fd = listen_local_fd;
-		listen[local].events = POLLIN;
-		for (int i = 0; i != 4; ++i) 
-		{
-				union {
-						sockaddr_in i;
-						sockaddr_un u;
-				} addr;
-				int stat;
-				do {
-					stat = poll(listen,2,-1);
-				} while (stat < 0);	
-				int listen_fd = -1;
-				if (listen[tcp].revents) listen_fd = listen_tcp_fd;
-				else if (listen[local].revents) listen_fd = listen_local_fd;
-				socklen_t s = sizeof(addr);
-				int fd = accept(listen_fd,(sockaddr*)&addr,&s);
-				register_fd(static_cast<player_id::type>(i),fd);
-				set_sock_options(fd);
-				LOG_PLACE() << " accepted " << i + 1 << " with fd = " << fd << ".\n";
-		}
-		close(listen_tcp_fd);
-		close(listen_local_fd);
+	int listen_tcp_fd = socket(PF_INET,SOCK_STREAM,0);
+	{
+		sockaddr_in addr;
+		memset(&addr,0,sizeof(addr));
+		addr.sin_family = AF_INET;
+		addr.sin_port = htons(options->tcp_port() );
+		addr.sin_addr.s_addr = htonl(INADDR_ANY);
+		set_sock_options(listen_tcp_fd);
+		if (bind(listen_tcp_fd,(sockaddr*)&addr,sizeof(addr)) < 0)
+			std::cerr << "Error in bind: " << strerror(errno) << ".\n";
+		listen(listen_tcp_fd,4);
+	}
+	int listen_local_fd = socket(PF_LOCAL,SOCK_STREAM,0);
+	{	
+		sockaddr_un addr;
+		memset(&addr,0,sizeof(addr));
+		addr.sun_family = AF_LOCAL;
+		unlink(options->unix_address() );
+		strcpy(addr.sun_path,options->unix_address() );
+		if (bind(listen_local_fd,reinterpret_cast<sockaddr*>(&addr),sizeof(addr)) < 0)
+			std::cerr << "Error in local bind: " << strerror(errno) << ".\n";
+		else listen(listen_local_fd,4);
+	}
+	pollfd listen[2];
+	memset(listen,0,sizeof(listen));
+	enum { tcp, local };
+	listen[tcp].fd = listen_tcp_fd;
+	listen[tcp].events = POLLIN;
+	listen[local].fd = listen_local_fd;
+	listen[local].events = POLLIN;
+	for (int i = 0; i != 4; ++i) 
+	{
+		union {
+			sockaddr_in i;
+			sockaddr_un u;
+		} addr;
+		int stat;
+		do {
+			stat = poll(listen,2,-1);
+		} while (stat < 0);	
+		int listen_fd = -1;
+		if (listen[tcp].revents) listen_fd = listen_tcp_fd;
+		else if (listen[local].revents) listen_fd = listen_local_fd;
+		socklen_t s = sizeof(addr);
+		int fd = accept(listen_fd,(sockaddr*)&addr,&s);
+		register_fd(static_cast<player_id::type>(i),fd);
+		set_sock_options(fd);
+		LOG_PLACE() << " accepted " << i + 1 << " with fd = " << fd << ".\n";
+	}
+	close(listen_tcp_fd);
+	close(listen_local_fd);
 }
+

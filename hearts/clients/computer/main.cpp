c7b0b13c0e46012cd49b94ebd1088bbedd838108
luis_pedro <luis_pedro@174c993c-7530-0410-8f2a-93c0592b7a1f>
2003-05-21 19:39:31 +0000

    If the zero option is given, we need to send one zero char to initialize the connection.
    This is not great design, but it works.
    
    
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@51 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts/clients/computer/main.cpp
+++ hearts/clients/computer/main.cpp
@@ -4,11 +4,19 @@
 #include "communication/open_connections.h"
 
 #include <iostream>
+#include <unistd.h>
 
 int main(int argc, char* argv[]) {
 	std::cerr << "HELLO\n";
 	int fd = open_client_connection(argc,argv);
 	Options::parse_args(argv);
+	if ( Options::send_zero() ) {
+		char zero = 0;
+		if ( write( fd, &zero, 1 ) < 0 ) {
+			std::cerr << "Sending initial zero failed.\n";
+			return 1;
+		}
+	}
 	ComputerPlayer player(fd);
 	while (Utility::Poll(player.get_pollfd(),Utility::Poll_Forever))
 	{

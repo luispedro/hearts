9f1415fa54f5b582bfdb7e80dc1da987c03783de
luis_pedro <luis_pedro@174c993c-7530-0410-8f2a-93c0592b7a1f>
2003-05-21 20:35:03 +0000

    Since private games are not implemented, don't show the option
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@58 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts/clients/human/setup/initialchoice.cpp
+++ hearts/clients/human/setup/initialchoice.cpp
@@ -9,11 +9,15 @@
 #include "general/widget_placement.h"
 #include "initialchoicewidget.h"
 
+
+#include <qradiobutton.h>
+
 	InitialChoice::InitialChoice(QWidget* parent, const char* name)
 		:SmartPage( parent, name )
 {
 	LOG_PLACE_NL();
 	widget_ = new InitialChoiceWidget( this );
+	widget_->radioPrivate->setEnabled( false );
 	LOG_PLACE_NL();
 	widget_->adjustSize();
 	widget_->move( 0,0 );
@@ -41,6 +45,8 @@ void InitialChoice::doNext( SetupWindow* parent )
 				parent->addPage( setup, "FIXME!!" );
 				parent->setFinishEnabled( setup, true );
 				parent->showPage( setup );
+				connect( parent, SIGNAL( execute() ), setup, SLOT( execute() ) );
+				connect( setup, SIGNAL( connected( int ) ),parent,SLOT( connected( int ) ) );
 				return;
 			}
 		case 2: // InitialChoice::Private:

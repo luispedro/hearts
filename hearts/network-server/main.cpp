a5a10deb32adc8aea9f763889b04a486d91c2163
luis_pedro <luis_pedro@174c993c-7530-0410-8f2a-93c0592b7a1f>
2003-05-22 21:28:30 +0000

    implemented a --daemon option
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@65 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts/network-server/main.cpp
+++ hearts/network-server/main.cpp
@@ -3,8 +3,18 @@
 #include "general/helper.h"
 
 
+#include <unistd.h>
+#include <iostream>
+#include <string>
+
 int main(int argc, char** argv)
 {
+	if ( argc > 1 && std::string( argv[ 1 ] ) == "--daemon" ) {
+		if ( daemon( 0, 0 ) < 0 ) {
+			std::cerr << "Daemon call failed: " << strerror( errno ) << ".\n";
+			return 1;
+		}
+	}
 	QApplication app( argc, argv, false ); // no GUI
 	( void )new Server( &app );
 	LOG_PLACE_NL();

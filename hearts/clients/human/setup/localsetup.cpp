b6d403f8f2b2eff8c4fe0bd11196c427d2994deb
luis <luis@174c993c-7530-0410-8f2a-93c0592b7a1f>
2003-08-31 13:22:14 +0000

    GUI fixes
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@110 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts/clients/human/setup/localsetup.cpp
+++ hearts/clients/human/setup/localsetup.cpp
@@ -32,54 +32,6 @@ LocalSetup::LocalSetup(QWidget* parent, const char* name)
 	:QWidget(parent,name),
 	 widget_( new LocalSetupWidget( this, "localsetupwidget" ) )
 {
-	/*
-	 self_message(new QLabel(i18n("Your name"),this)),
-	 self_name(new QLineEdit(Options::playerName(player_id::self),this)),
-	 right_message(new QLabel(i18n("Name of opponent on right"),this)),
-	 right_name(new QLineEdit(Options::playerName(player_id::right),this)),
-	 front_message(new QLabel(i18n("Name of opponent in front"),this)),
-	 front_name(new QLineEdit(Options::playerName(player_id::front),this)),
-	 left_message(new QLabel(i18n("Name of opponent on left"), this)),
-	 left_name(new QLineEdit(Options::playerName(player_id::left),this)),
-	 go(new QPushButton(i18n("Go!"),this))
-{
-	connect(go,SIGNAL(clicked()),SLOT(execute()));
-	
-	
-	const int x = 10;
-	const int padding = 10;
-	
-	self_message->move(x,padding);
-	self_message->setMinimumSize(self_message->sizeHint());
-	LOG_PLACE() << self_message->sizeHint().width() << ',' << self_message->sizeHint().height() << '\n';
-	int max_x = right_of(self_message);
-	
-	right_message->move(x,below(self_message) + padding);
-	adjust_size(right_message);
-	max_x = std::max(max_x,right_of(right_message));
-	
-	front_message->move(x,below(right_message) + padding);
-	adjust_size(front_message);
-	max_x = std::max(max_x,right_of(front_message));
-
-	left_message->move(x,below(front_message) + padding);
-	adjust_size(left_message);
-	max_x = std::max(max_x, right_of(left_message));
-
-	max_x += padding;
-
-#define ADJUST(x) x##_name->setMinimumSize(140,x##_name->height());x##_name->move(max_x,x##_message->y());
-
-	ADJUST(self);
-	ADJUST(right);
-	ADJUST(front);
-	ADJUST(left);
-	
-	go->move(x,below(left_message) + padding);
-
-	setMinimumSize(right_of(self_name) + padding, below(go) + padding);
-	LOG_PLACE() << right_of(self_name) + padding << ',' << below(go) + padding << ")\n";
-*/	
 }
 
 

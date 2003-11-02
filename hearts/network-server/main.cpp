38925548c75a4f321451412cc0e3a88c1563bbca
luis <luis@174c993c-7530-0410-8f2a-93c0592b7a1f>
2003-11-02 18:37:36 +0000

    Write a .pid file like a big boy.
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@134 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts/network-server/main.cpp
+++ hearts/network-server/main.cpp
@@ -5,7 +5,17 @@
 #include <errno.h>
 #include <unistd.h>
 #include <iostream>
+#include <fstream>
 #include <string>
+
+void write_pid_file()
+{
+	std::ofstream pidf( "/var/run/hearts.pid" );
+	pidf << getpid() << std::endl;
+	if ( !pidf ) {
+		std::cerr << "Error writing pid file" << std::endl;
+	}
+}
 
 int main(int argc, char** argv)
 {
@@ -15,6 +25,7 @@ int main(int argc, char** argv)
 			return 1;
 		}
 	}
+	write_pid_file();
 	QApplication app( argc, argv, false ); // no GUI
 	( void )new Server( &app );
 	LOG_PLACE_NL();

1382916e9ff6359d51bb3a3c516513dfbdca373c
luis <luis@174c993c-7530-0410-8f2a-93c0592b7a1f>
2003-11-02 18:16:40 +0000

    Change the server. We now run on hearts.luispedro.org.
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@132 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts/common/network/constants.h
+++ hearts/common/network/constants.h
@@ -1,5 +1,8 @@
 namespace Network {
-	const char * const Server = "hearts.sinus.cz";
+	// TODO:
+	// This shouldn't be in the code
+	// Something like a .desktop file should hold this information
+	const char * const Server = "hearts.luispedro.org";
 	const short Port = 1443;
 }
 

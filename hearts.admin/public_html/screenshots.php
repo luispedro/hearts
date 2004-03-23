e3b516feac87f43450f2f8cfa7d90c80b253da95
luis <luis@174c993c-7530-0410-8f2a-93c0592b7a1f>
2004-03-23 14:24:14 +0000

    Use 480/*png and not *Small.png for thumbs
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@193 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts.admin/public_html/screenshots.php
+++ hearts.admin/public_html/screenshots.php
@@ -7,13 +7,13 @@ do_header('Screenshots');
 This the game going on.
 
 </TD>
-<TD><p><a href="screenshots/Playing.png"><img src="screenshots/PlayingSmall.png"></a>
+<TD><p><a href="screenshots/Playing.png"><img src="screenshots/480/Playing.png"></a>
 <TR>
 <TD>
 This is when you pass cards at the beginning of a game.
 
 </TD>
-<TD><p><a href="screenshots/Passing.png"><img src="screenshots/PassingSmall.png"></a>
+<TD><p><a href="screenshots/Passing.png"><img src="screenshots/480/Passing.png"></a>
 </TR>
 
 
@@ -21,13 +21,13 @@ This is when you pass cards at the beginning of a game.
 <TD>
 This is the complete picture, there is the main window, the score window and the options window visible.
 </TD>
-<TD><p><a href="screenshots/Full.png"><img src="screenshots/FullSmall.png"></a>
+<TD><p><a href="screenshots/Full.png"><img src="screenshots/480/Full.png"></a>
 
 <TR>
 <TD>
 This is the new version. You can see the dialog where you configure what kind of game you play (network or local).
 </TD>
-<TD><p><a href="screenshots/NetworkSelection.png"><img src="screenshots/NetworkSelectionSmall.png"></a>
+<TD><p><a href="screenshots/NetworkSelection.png"><img src="screenshots/480/NetworkSelection.png"></a>
 </TABLE>
 
 <?php do_footer();?>

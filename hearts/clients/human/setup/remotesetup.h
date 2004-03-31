da0ed582707844234bc871938306b7c25f18e990
luis <luis@174c993c-7530-0410-8f2a-93c0592b7a1f>
2004-03-31 19:26:28 +0000

    As with the other widgets here, have all the GUI in a thingWidget and the logic in thing.
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@216 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts/clients/human/setup/remotesetup.h
+++ hearts/clients/human/setup/remotesetup.h
@@ -2,25 +2,20 @@
 #define DB_REMOTESETUP_H_INCLUDE_GUARD_
 
 #include <qwidget.h>
-class QPushButton;
-class QLabel;
-class QLineEdit;
+class RemoteSetupWidget;
+
 
 class RemoteSetup : public QWidget
 {
 		Q_OBJECT
 	public:
-		RemoteSetup( QWidget* parent, QString name = "remote-setup" );
+		RemoteSetup( QWidget* parent, const char* name = "remote-setup" );
 	signals:
 		void connected( int );
 	public slots:
 		void execute();
 	private:
-		QPushButton* ok;
-		QLabel* ip_label;
-		QLineEdit* ip;
-		QLabel*	name_label;
-		QLineEdit*	name;
+		RemoteSetupWidget* widget_;
 };
 
 

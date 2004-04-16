47ce55b42f9078bcd62430e763c7f9a047dbe25c
luis <luis@174c993c-7530-0410-8f2a-93c0592b7a1f>
2004-04-16 20:28:52 +0000

    Show what's done in CVS Fix the wording on some items. Add learning-AI idea.
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@289 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts.admin/public_html/plan.php
+++ hearts.admin/public_html/plan.php
@@ -1,36 +1,43 @@
 <?php
 		include('base.php');
 		do_header('Hearts\' Development Plan');
+
+		function cvsdone() {
+				print '[FIXED IN <a href="download.php#CVS">CVS</a>]';
+		}
 ?>
 
 <p>This is basically what I plan to do. There are no dates assigned to any of the steps,
 	and there is no guarantee that I will keep the plan 100%, but here it is.</p>
+
+<p>Latest update: <b>16th April 2004</b></p>
 
 <h2>Version 1.98</h2>
 
 <ul>
 	<li>Fix all known bugs:
 		<ul>
-			<li>The card passing is always to the right.
-			<li>Portability issue regarding setting the random seed.
-			<li>A lot of processes are left running.
-			<li>One one match (ie, a series of games until a player gets 100 points) is possible.
+			<li>The card passing is always to the right. <?=cvsdone()?>
+			<li>Portability issue regarding setting the random seed. <?=cvsdone()?>
+			<li>A lot of processes are left running. <?=cvsdone()?>
+			<li>Only one match (ie, a series of games until a player gets 100 points) is possible.
 			<li>Make it valgrind clean.
 		</ul>
-	<li>Have the status bar help on what the user should do at every step (play, give cards right, etc.)
+	<li>Have the status bar help on what the user should do at every step (play, give cards right, etc.) <?=cvsdone()?>
 </ul>
 
 <h2>Version 1.99</h2>
 
 <ul>
 <li>Make Network Gaming a reality, even if it's very simple.
-<li>Show who's online, a Message of the day, have some stats. This is mostly back-end work
+<li>Show who's online, a Message of the day, have some stats (this is mostly back-end work).
 </ul>
 
 <h2>Version 2.0 </h2>
 <ul>
-<li>Add some simple bells and whistles in the Interface.
-<li>Make the AI players just a bit less stupid.
+<li>Make the interface more complete (a Menu Bar with common options).
+<li>Write Documentation.
+<li>Make the AI players less stupid.
 </ul>
 
 <h2>Version 2.1 -- The future</h2>
@@ -43,6 +50,7 @@
 	<li>Show what cards the players played at the end of the game
 	<li>Plug in to kopete (or similar) for setting up network games
 	<li>Clean up the code
+	<li>Make good AI players using learning algorithms
 </ul>
 
 

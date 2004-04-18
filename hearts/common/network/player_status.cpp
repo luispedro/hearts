4c065624b9c8b4854163ad1156eb8489c5492f55
luis <luis@174c993c-7530-0410-8f2a-93c0592b7a1f>
2004-04-18 18:23:32 +0000

    To pass around a players' status, a small enum
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@314 174c993c-7530-0410-8f2a-93c0592b7a1f

---
+++ hearts/common/network/player_status.cpp
@@ -0,0 +1,22 @@
+#include "player_status.h"
+
+
+std::ostream& operator << ( std::ostream& out, player_status::type s ) {
+		switch ( s ) {
+#define CASE( x ) case player_status::x: return out << #x;
+				FOR_ALL_PLAYER_STATUS( CASE );
+#undef CASE
+				default:
+					std::cerr << "Unknonwn player_status (" << static_cast<int>( s ) << ")\n";
+					return out << "(unknown)";
+		}
+}
+std::istream& operator >> ( std::istream& in, player_status::type& s ) {
+		std::string str;
+		in >> str;
+#define CASE( x ) if ( str == #x ) { s = player_status::x; return in; }
+		FOR_ALL_PLAYER_STATUS( CASE )
+		in.setstate( std::ios::failbit );
+		return in;
+}
+
---
+++ hearts/common/network/player_status.h
@@ -0,0 +1,27 @@
+#ifndef LPC_PLAYER_STATUS_H1082312546_INCLUDE_GUARD_
+#define LPC_PLAYER_STATUS_H1082312546_INCLUDE_GUARD_
+
+#include <iostream>
+
+#define FOR_ALL_PLAYER_STATUS( F ) \
+		F( online ) \
+		F( playing ) \
+		F( waiting ) \
+		F( loggout )
+		
+
+namespace player_status {
+		enum type {
+#define ARG_COMMA( x ) x,
+				FOR_ALL_PLAYER_STATUS( ARG_COMMA )
+				unknown
+#undef ARG_COMMA
+		};
+}
+
+std::ostream& operator << ( std::ostream&, player_status::type );
+std::istream& operator >> ( std::istream&, player_status::type& );
+
+
+
+#endif /* LPC_PLAYER_STATUS_H1082312546_INCLUDE_GUARD_ */

b42d559f9de79316e2d449ed74edfe082a63ab03
luis <luis@174c993c-7530-0410-8f2a-93c0592b7a1f>
2004-07-27 00:38:31 +0000

    Differentiate between FIXED and DONE Make the plan for 1.99 more detailed (a real plan and not an intention).
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@345 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts.admin/public_html/plan.php
+++ hearts.admin/public_html/plan.php
@@ -2,34 +2,41 @@
 		include('base.php');
 		do_header('Hearts\' Development Plan');
 
-		function cvsdone() {
+		function cvsfixed() {
 				print '[FIXED IN <a href="download.php#CVS">CVS</a>]';
+		}
+		function cvsdone() {
+				print '[DONE IN <a href="download.php#CVS">CVS</a>]';
 		}
 ?>
 
 <p>This is basically what I plan to do. There are no dates assigned to any of the steps,
 	and there is no guarantee that I will keep the plan 100%, but here it is.</p>
 
-<p>Latest update: <b>16th April 2004</b></p>
+<p>Latest update: <b>27th July 2004</b></p>
 
 <h2>Version 1.98</h2>
 
 <ul>
 	<li>Fix all known bugs:
 		<ul>
-			<li>The card passing is always to the right. <?=cvsdone()?>
-			<li>Portability issue regarding setting the random seed. <?=cvsdone()?>
-			<li>A lot of processes are left running. <?=cvsdone()?>
-			<li>Only one match (ie, a series of games until a player gets 100 points) is possible. <?=cvsdone()?>
+			<li>The card passing is always to the right. <?=cvsfixed()?>
+			<li>Portability issue regarding setting the random seed. <?=cvsfixed()?>
+			<li>A lot of processes are left running. <?=cvsfixed()?>
+			<li>Only one match (ie, a series of games until a player gets 100 points) is possible. <?=cvsfixed()?>
 		</ul>
 	<li>Have the status bar help on what the user should do at every step (play, give cards right, etc.) <?=cvsdone()?>
 </ul>
 
 <h2>Version 1.99</h2>
 
+<p>The focus is on Network Gaming, namelly:</p>
+
 <ul>
-<li>Make Network Gaming a reality, even if it's very simple.
-<li>Show who's online, a Message of the day, have some stats (this is mostly back-end work).
+<li>Show who's online <?=cvsdone()?>
+<li>Message of the Day
+<li>Allow Registration (and Authentication)
+<li>Generate Statistics
 </ul>
 
 <h2>Version 2.0 </h2>

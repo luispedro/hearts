c63be011f68a5470ae360d7a945b33618f9e66e1
luis <luis@174c993c-7530-0410-8f2a-93c0592b7a1f>
2004-09-05 19:16:37 +0000

    Include guards
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@371 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts/network-server/logfile.h
+++ hearts/network-server/logfile.h
@@ -1,4 +1,10 @@
+#ifndef LPC_LOGFILE_H1094411175_INCLUDE_GUARD_
+#define LPC_LOGFILE_H1094411175_INCLUDE_GUARD_
+
 #include <ostream>
 
 void reopenlog();
 std::ostream& logfile();
+
+
+#endif /* LPC_LOGFILE_H1094411175_INCLUDE_GUARD_ */

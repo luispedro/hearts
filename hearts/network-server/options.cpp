984d5a85a2d55a0f4aab58c934dfd9db434041e5
luis <luis@174c993c-7530-0410-8f2a-93c0592b7a1f>
2004-09-05 19:27:15 +0000

    Switch to using configuration file based options
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@373 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts/network-server/options.cpp
+++ hearts/network-server/options.cpp
@@ -3,6 +3,8 @@
 #include <getopt.h>
 #include <iostream>
 #include <cassert>
+#include <memory>
+#include <fstream>
 
 namespace
 {
@@ -11,48 +13,80 @@ namespace
 
 const Options* &options = const_cast<const Options*&>( singleton );
 
-Options::Options()
-	:daemon_( false )
+
+
+optionsDescrition heartsdOptions[] = {
+	{ "database", { "host", "username", "password", "database", 0 } },
+	{ "misc", { "pidfile", "logfile", "motdfile", 0 } },
+	{ 0, {} }
+};
+
+Options::Options( std::istream& in )
+	:daemon_( false ),
+	 parsed_( in, heartsdOptions )
 {
 }
 
 extern char* optarg;
 
+
+template<unsigned N, typename T>
+unsigned numelems( T ( & )[ N ] ) { return N; }
+
+std::auto_ptr<std::ifstream> findConfFile() {
+	const char* locations[] = { "/etc/heartsconf", "/etc/hearts.conf", "/etc/hearts/conf" };
+
+	for ( const char** iter = locations; iter != locations + numelems( locations ); ++iter ) {
+		std::auto_ptr<std::ifstream> res( new std::ifstream( *iter ) );
+		if ( *res ) return res;
+	}
+	return std::auto_ptr<std::ifstream>( 0 );
+}
+
 void Options::init( int argc, char** argv )
 {
 	assert( !singleton );
-	singleton = new Options;
 
-	enum { daemon = 1, logfile, pidfile, motdfile };
+	enum { daemon = 1, conffile };
 	static const struct option long_options[] = {
 			{ "daemon", 0, 0, daemon },
 			{ "deamon", 0, 0, daemon },
-			{ "logfile", 1, 0, logfile },
-			{ "pidfile", 1, 0, pidfile },
-			{ "motdfile", 1, 0, motdfile },
+			{ "conffile", 1, 0, conffile },
 			{ 0, 0, 0, 0 }
 	};
 
 	int option_index = 0;
 	int res;
+	std::string confname;
 	while ( ( res = getopt_long( argc, argv, "", long_options, &option_index ) ) > 0 ) {
-		const char* cur = optarg;
 		switch ( res ) {
 			case daemon:
 				singleton->daemon_ = true;
 				break;
-			case logfile:
-				singleton->logFile_ = std::string( cur );
+			case conffile: 
+				confname = optarg;
 				break;
-			case pidfile:
-				singleton->pidFile_ = std::string( cur );
-				break;
-			case motdfile:
-				singleton->motdFile_ = std::string( cur );
 			default:
 				std::cerr << "Strange return from getopt (" << char( res ) << ")\n";
 		}
 	}
-}
 
+	std::auto_ptr<std::ifstream> conf;
+
+	if ( confname.empty() ) {
+		conf = findConfFile();
+	} else {
+		conf.reset( new std::ifstream( confname.c_str() ) );
+		if ( !*conf ) {
+			std::cerr << "Unable to open configuration file \'" << confname.c_str() << "\'\n";
+			exit( 1 );
+		}
+	}
+	if ( conf.get() ) {
+		singleton = new Options( *conf );
+	} else {
+		std::cerr << "Configuration file not found!\n";
+		exit( 1 );
+	}
+}
 

091989429c17d50ae41f46ca96e69c6f6c97d4bc
luis <luis@174c993c-7530-0410-8f2a-93c0592b7a1f>
2004-09-06 13:30:04 +0000

    The server was never supposed to be here
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@379 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts/common/network/constants.h
+++ hearts/common/network/constants.h
@@ -3,10 +3,6 @@
 
 namespace Network
 {
-// TODO:
-// This shouldn't be in the code
-// Something like a .desktop file should hold this information
-const char * const Server = "hearts.luispedro.org";
 const short Port = 1443;
 }
 

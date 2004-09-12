d61e655b1248d30eebc24960774413bbc0aa896c
luis <luis@174c993c-7530-0410-8f2a-93c0592b7a1f>
2004-09-12 13:04:14 +0000

    Fix query string Remember notify setting
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@389 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts.admin/public_html/register.php
+++ hearts.admin/public_html/register.php
@@ -17,9 +17,12 @@ if (isset($r_name)) {
 		$r_pwd = $r_pwd2 = '';
 	} else { // ok
 		$handle = mysql_connect($config_db_host, $config_db_user, $config_db_pwd);
-		mysql_select_db($config_db_db);
 		if (!$handle) {
 			die('Connection to database failed: ' . mysql_error());
+		}
+		mysql_select_db($config_db_db);
+		if (!$handle) {
+			die('Select database failed: ' . mysql_error());
 		}
 		$res = mysql_query("SELECT * FROM users WHERE username = '$r_name'",$handle);
 		if (!$res) {
@@ -40,7 +43,8 @@ if (isset($r_name)) {
 				$notify = 'FALSE';
 				if ($r_notify == 'on') $notify = 'TRUE';
 				mysql_query("INSERT INTO users(email,username,password,lastLogin,notifyreleases) " .
-						"VALUES('$r_email','$r_name','$r_password',NOW(),'$notify)");
+						"VALUES('$r_email','$r_name','$r_password',NOW(),'$notify')")
+				or die ('Error: ' . mysql_error());
 				$done = true;
 			}
 		}
@@ -57,13 +61,14 @@ if (!$done) {
 		}
 	?>
 
-	<form action="do-register.php" method="get">
+	<form action="register.php" method="get">
 	<table>
 	<tr><td>Nickname wanted</td><td><input type="text" name="name" value="<?=$r_name?>" />
 	<tr><td>Email</td><td><input type="text" name="email" value="<?=$r_email?>" />
 	<tr><td>Password</td><td><input type="password" name="pwd" value="<?=$r_pwd?>" />
 	<tr><td>Password (again)</td><td><input type="password" name="pwd2" value="<?=$r_pwd2?>" />
-	<tr><td>Notification of releases</td><td><input type="checkbox" value="<?=$r_notify?>" name="notify" />
+	<tr><td>Notification of releases</td><td>
+			<input type="checkbox" <?=($r_notify == "on")?'checked=true':''?> name="notify" />
 	<tr><td /><td><input type="submit" value="Register" />
 	</table>
 	</form>

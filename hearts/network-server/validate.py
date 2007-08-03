4de58503ab1f3a56391e5bf4f13fa1dc88e32f2d
luispedrocoelho <luispedrocoelho@174c993c-7530-0410-8f2a-93c0592b7a1f>
2007-08-03 22:54:58 +0000

    Correct bugs.
    git-svn-id: file:///home/luispedro/luispedro.org/svn/hearts/trunk@450 174c993c-7530-0410-8f2a-93c0592b7a1f

--- hearts/network-server/validate.py
+++ hearts/network-server/validate.py
@@ -6,23 +6,49 @@
 # Distributed under GNU Public License, version 2
 import md5
 
-def _do_md5(s):
+def _do_md5_hex(s):
     M=md5.new()
     M.update(s)
     return M.hexdigest()
 
-def _char_xor(ca,cb):
-    return hex(int(ca,16) ^ int(cb,16))[-1]
-def _string_xor(sa,sb):
-    return "".join([_char_xor(ca,cb) for ca, cb in zip(sa,sb)])
+def _do_md5(s):
+    M=md5.new()
+    M.update(s)
+    return M.digest()
 
-def validate(method,cookie,pwd,result):
-    if method == 'md5(md5(cookie)*md5(result))':
+
+def _string_xor(sa,sb):
+    return "".join([chr( ord(ca) ^ ord(cb) ) for ca, cb in zip(sa,sb)])
+
+def validate(method,cookie,pwd,key):
+    """
+    validate(method, cookie, pwd, key)
+
+    Returns whether the key fits the (cookie,pwd) under method.
+    @param method: Algorithm to use. Currently only md5(md5(pass)*md5(cookie)) is supported.
+    @param cookie: The cookie used
+    @param pwd: The actual password
+    @param key: The key given by the user
+    """
+    print "pwd\t\t", pwd
+    if method == 'md5(md5(pass)*md5(cookie))':
         mcookie=_do_md5(cookie)
-        mpwd=_do_md5(result)
-        expected=_do_md5(_string_xor(mcookie,mpwd))
-        return expected == result
+        mpwd=_do_md5(pwd)
+        expected=_do_md5_hex(_string_xor(mcookie,mpwd))
+        print "key\t\t", key
+        print "expected\t", expected
+        return expected == key
     else:
         raise Exception("validate(): Unkown method '%s'" % method)
 
+if __name__ == '__main__':
+    tcookie='chocolate-chip'
+    tpwd='big-secret'
+    tres=_do_md5_hex(_string_xor(_do_md5(tcookie),_do_md5(tpwd)))
+    assert validate('md5(md5(pass)*md5(cookie))',tcookie,tpwd,tres)
+    tres=_do_md5_hex(_string_xor(_do_md5(tpwd),_do_md5(tcookie)))
+    assert validate('md5(md5(pass)*md5(cookie))',tcookie,tpwd,tres)
+    tres=_do_md5_hex(_string_xor(_do_md5(tcookie),_do_md5(tcookie)))
+    assert not validate('md5(md5(pass)*md5(cookie))',tcookie,tpwd,tres)
+    print 'OK'
 # vim: set ts=4 sts=4 sw=4 expandtab smartindent:
